3181858a1b9b89500c716260e6bdc9e004425d21
Paddy Atack <monman13@Paddys-MacBook-Pro.local>
2025-10-24 15:58:17 +0100

    next config + cloudflare adapter

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
-// next.config.mjs
 import { defineConfig } from 'next';
 
 export default defineConfig({
-  // Don’t let ESLint/TS stop the build while we’re moving fast
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 });
